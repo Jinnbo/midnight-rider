5861d3511040c4c1d5f7ee2136577a1a314c4290
Jimmy Hui <jimmyhui92012@gmail.com>
2021-01-07 09:57:30 -0800

    Added Refuel

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 # A text-based adventure game
 # Gamespot gives it 9 out of 10
 
+import random
 import sys
 import textwrap
 import time
+
 
 INTRODUCTION = """
 WELCOME TO MIDNIGHT RIDER
@@ -26,6 +28,7 @@ REACH THE END BEFORE THE MAN GON GETCHU.
 
 CHOICES = """
     ---
+    D. Stop at gas station
     E. Status Check
     Q. QUIT
     ___
@@ -42,6 +45,7 @@ def intro():
 
 def main():
     #intro()
+    MAX_FUEL_LEVEL = 50
 
     # Variables
     done = False
@@ -50,7 +54,7 @@ def main():
     agents_distance = -20 # 0 is the end
     turns = 0             #
     tofu = 3              # 3 is max
-    fuel = 50             # Max is 50 L
+    fuel = MAX_FUEL_LEVEL # Max is 50 L
     hunger = 0            #
 
 
@@ -65,7 +69,23 @@ def main():
 
         user_choice = input("What do you want to do? ").lower().strip(",./<>?!")
 
-        if user_choice == "e":
+
+        if user_choice == "d":
+            # Refueling
+            # Fill up the fuel tank
+            fuel = MAX_FUEL_LEVEL
+
+            # Consider the agents coming closer
+            agents_distance += random.randrange(7, 15)
+
+            # Give player feedback
+            print()
+            print("-------- You filled the fuel tank --------")
+            print("-------- The agents got closer --------")
+            print()
+
+
+        elif user_choice == "e":
             print(f"\t-----Status Check-----")
             print(f"\tkm travelled: {kms_travelled}")
             print(f"\tFuel remaining: {fuel} L")
